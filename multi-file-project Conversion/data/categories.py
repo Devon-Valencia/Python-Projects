557004adb92409440ebec370359d15d2a9fdff5b
devon-valencia <devonval06@gmail.com>
2025-02-08 00:53:37 -0700

    Converted my multi file project from JavaScript to Python, while learning the basics of Python.

---
+++ multi-file-project Conversion/data/categories.py
@@ -0,0 +1,6 @@
+catagories = [
+    { "id": 1, "name": "Food" },
+    { "id": 2, "name": "Clothing" },
+    { "id": 3, "name": "Books" },
+    { "id": 4, "name": "Bikes"}
+  ]
---
+++ multi-file-project Conversion/data/products.py
@@ -0,0 +1,9 @@
+products = [
+    { "name": "Chocolate", "price": 5, "categoryid": 1 },
+    { "name": "T-Shirt", "price": 15, "categoryid": 2 },
+    { "name": "Node.js Guide", "price": 20, "categoryid": 3 },
+    { "name": "Willy the whale", "price": 17, "categoryid": 3 },
+    { "name": "Cilford the Big Red Dog", "price": 10, "categoryid": 3 },
+    { "name": "Sword Art Online Manga", "price": 45, "categoryid": 3 },
+    { "name": "Pasta", "price": 8, "categoryid": 1 }
+  ]
